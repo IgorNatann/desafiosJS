e1d62526f2a6e7c2d8be5fb247c3ce2f9821b032
Igor Natan <igornatan4@gmail.com>
2022-02-15 16:03:46 -0300

    Commit dos desafios curso!

---
+++ JS/desafio2.js
@@ -0,0 +1,9 @@
+/* Desafio
+
+Você receberá dois valores inteiros. Faça a leitura e em seguida calcule o produto entre estes dois valores.
+Atribua esta operação à variável PROD, mostrando esta de acordo com a mensagem de saída esperada (exemplo abaixo). */  
+
+let valor1 = parseInt(gets(3));
+let valor2 = parseInt(gets(9));
+let total = valor1 * valor2; // Altere o valor da variável com o cálculo esperado 
+console.log("PROD = " + total);
